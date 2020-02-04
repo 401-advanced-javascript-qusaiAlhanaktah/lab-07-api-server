85543afbdb8b4f8560fe32226691c7cdecd9e490
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-04 13:40:24 +0200

    solve the timeStamp middle ware

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,7 +1,6 @@
-'use strict';
+
 
 module.exports = (req, res, next) => {
-  console.log('request info:', req.method, req.path);
-  console.log( 'the time is: ',new Date());
+  console.log('request info:', req.method, req.path, req.timeStamp);
   next();
-}
+};
